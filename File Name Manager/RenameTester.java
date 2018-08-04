6adea1c59e06c212889c615493d6649377cff939
gregneat <gneat@gusd.net>
2018-08-04 10:59:29 -0700

    Revert "testing ricky file naming code 2"
    This reverts commit fd432d0d5d08d6373de40fde161c36007a300caa.

--- File Name Manager/RenameTester.java
+++ File Name Manager/RenameTester.java
@@ -5,14 +5,14 @@ public class RenameTester
 {
     public static void main(String[] argv) throws IOException
     {
-        // Path of folder where files are located
-        String folder_path = "\\C:\\Users\\gneat\\Desktop\\testNum\\";
-		
+		// Path of folder where files are located
+        // String folder_path = "C:\\Users\\ricky\\Desktop\\Java\\ACD19";
+		String folder_path = "C:\\Users\\gneat\\Desktop\\testNum";
         // creating new folder
         File myfolder = new File(folder_path);
-//Renamer.shiftFileOrder(myfolder, 1, 3);
+		// Renamer.shiftFileOrder(myfolder, 20, 30);
 		// Renamer.renameFilesInBinary(myfolder);
-		//Renamer.renameFilesInBase10(myfolder);
+		// Renamer.renameFilesInBase10(myfolder);
 		System.out.println("Files have been changed");
     }
 }
